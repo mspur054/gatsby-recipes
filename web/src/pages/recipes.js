9bac4701132569d28d2fa71976cc6392b78379c7
Matthew Spurdle <mspur054@uottawa.ca>
2021-03-03 19:45:29 -0500

    fixed layout of recipes pages grid.

--- web/src/pages/recipes.js
+++ web/src/pages/recipes.js
@@ -5,6 +5,9 @@ import Layout from "../components/layout"
 
 import recipeStlyes from './recipes.module.scss'
 
+
+
+
 const RecipePage = () => {
   const data = useStaticQuery(graphql`
     query {
@@ -26,20 +29,26 @@ const RecipePage = () => {
       <h1>Recipes</h1>
       <p>Recipes show up under here</p>
       <div className={recipeStlyes.recipes_top}>
-      <div className={recipeStlyes.filters}>
-      <h2>Filter Recipes</h2>
-      </div>
-      <ol className={recipeStlyes.recipes}>
-      {data.allSanityRecipe.edges.map(({node}) =>{
-          return (
-              <li className={recipeStlyes.recipe} key={node.slug.current}>
-                <Link className={recipeStlyes.item} to={`/recipes/${node.slug.current}`}>
+        <div className={recipeStlyes.filters}>
+          <h2>Filter Recipes</h2>
+        </div>
+        <div className={recipeStlyes.recipes}>
+          {data.allSanityRecipe.edges.map(({ node }) => {
+            return (
+              <Link key={node.slug.current} className={recipeStlyes.recipe} to={`/recipes/${node.slug.current}`}>
+              <article className={recipeStlyes.item} key={node.slug.current}>
+                <figure></figure>
+                <div className={recipeStlyes.textArea}>
+         
+                 
                     <h2 className={recipeStlyes.header}>{node.name}</h2>
-                    <p>{node.description}</p>
-                </Link>
-              </li>
-          )
-      })}</ol>
+                    {/* <p>{node.description}</p> */}
+                  
+                </div>
+              </article>
+              </Link>
+            )
+          })}</div>
       </div>
     </Layout>
   )

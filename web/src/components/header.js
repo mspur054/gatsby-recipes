3e02cf10f163c155ac0d17cb5002e747d4f1f6cd
Matthew Spurdle <mspur054@uottawa.ca>
2021-03-03 20:22:47 -0500

    fixed 'class' bug

--- web/src/components/header.js
+++ web/src/components/header.js
@@ -14,29 +14,32 @@ const Header = () => {
       }
     }
     `)
-    return(
+    return (
         <header className={headerStyles.header}>
             <div className={headerStyles.headerwrap}>
-            <h1>
-            <Link className={headerStyles.title} to='/'>
-                
-        {data.site.siteMetadata.title}
-        </Link>
-            </h1>
+                <h1>
+                    <Link className={headerStyles.title} to='/'>
+
+                        {data.site.siteMetadata.title}
+                    </Link>
+                </h1>
             </div>
             <nav>
+                <span className="navbar-toggle" id="js-navbar-toggle">
+                    <i className="las la-bars"></i>
+                </span>
                 <div className={headerStyles.subnav}>
-                <ul className={headerStyles.navList}>
-                    <li>
-                        <Link className={headerStyles.navItem} activeClassName={headerStyles.activeNavItem} to='/'>Home</Link>
-                    </li>
-                    <li>
-                        <Link className={headerStyles.navItem} activeClassName={headerStyles.activeNavItem} to='/recipes'>Recipes</Link>
-                    </li>
-                    <li>
-                        <Link className={headerStyles.navItem} activeClassName={headerStyles.activeNavItem} to='/about'>About</Link>
-                    </li>
-                </ul>
+                    <ul className={headerStyles.navList}>
+                        <li>
+                            <Link className={headerStyles.navItem} activeClassName={headerStyles.activeNavItem} to='/'>Home</Link>
+                        </li>
+                        <li>
+                            <Link className={headerStyles.navItem} activeClassName={headerStyles.activeNavItem} to='/recipes'>Recipes</Link>
+                        </li>
+                        <li>
+                            <Link className={headerStyles.navItem} activeClassName={headerStyles.activeNavItem} to='/about'>About</Link>
+                        </li>
+                    </ul>
                 </div>
             </nav>
         </header>
